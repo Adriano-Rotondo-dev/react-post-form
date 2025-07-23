f73995129f431d6cb704b94cb2b3ee944ca5d437
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-07-23 15:50:20 +0200

    ass base structure of post page, install and import axios library

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,82 @@
 import { useState } from "react";
+import axios from "axios";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      {/* container  */}
+      <div className="container mb-3">
+        {/* card section  */}
+        <div className="card m-3">
+          {/* start of imput section  */}
+          <div className="p-3">
+            <label htmlFor="" className="form-label">
+              Author
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              name="author"
+              id="author"
+              aria-describedby="helpId"
+              placeholder="Type here..."
+            />
+            <small id="helpId" className="form-text text-muted">
+              Type here the author's name
+            </small>
+          </div>
+          <div className="p-3">
+            <label htmlFor="" className="form-label">
+              Title
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              name="title"
+              id="title"
+              aria-describedby="helpId"
+              placeholder="Type here..."
+            />
+            <small id="helpId" className="form-text text-muted">
+              Type here the post's title
+            </small>
+          </div>
+          <div className="p-3">
+            <label htmlFor="" className="form-label">
+              Content
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              name="post"
+              id="post"
+              aria-describedby="helpId"
+              placeholder="Type here..."
+            />
+            <small id="helpId" className="form-text text-muted">
+              Type here your post
+            </small>
+          </div>
+          {/* end of imput section  */}
+          {/* checkbox section  */}
+          <div class="form-check m-3">
+            <input
+              class="form-check-input"
+              type="checkbox"
+              value=""
+              id=""
+              checked
+            />
+            <label class="form-check-label" for="">
+              Is your post public?
+            </label>
+          </div>
+        </div>
+        {/* closing tag for card */}
+      </div>
+      {/* closing tag for container */}
+    </>
+  );
 }
 
 export default App;
